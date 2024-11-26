4bbaa6b7e88ea54cfe752b7680c127f719f85c6d
fadhil riyanto <me@fadev.org>
2024-11-26 21:18:26 +0700

    test append

--- phpman.c
+++ phpman.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
-int main()
+#include "modules/log.c-patched/src/log.h"
+#include "dbref.h"
+
+static int _main(char* strname)
 {
-        printf("hello world\n");
+        struct heap_control heap_ctx;
+        mkkeyvalue_heap(1, &heap_ctx);
+
+        add_record2_heap(&heap_ctx, "abc", "def");
+        add_record2_heap(&heap_ctx, "abc", "def");
+        add_record2_heap(&heap_ctx, "abcd", "ddef");
+        
+
+}
+
+int main(int argc, char** argv)
+{
+
+        if (argc == 2) {
+                
+
+                /* start entry */
+                _main(argv[1]);
+        } else {
+                log_error("%s\n", "arg is null, please spesify the function name");
+        }
 }
