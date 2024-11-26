fc149bccdc044981a65cc230265e74170b8a08b3
fadhil riyanto <me@fadev.org>
2024-11-26 21:46:07 +0700

    add function search

--- phpman.c
+++ phpman.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 #include "modules/log.c-patched/src/log.h"
 #include "dbref.h"
 
+static void append_dictionary(struct heap_control *heap_ctx)
+{
+        add_record2_heap(heap_ctx, "ZEND_PARSE_PARAMETERS_START", 
+                "ZEND_PARSE_PARAMETERS_START(num_required_args, num_max_args)" \
+                "takes two arguments minimal and maximal parameters count."
+                );
+        
+}
+
 static int _main(char* strname)
 {
+        short found = 0;
+
         struct heap_control heap_ctx;
         mkkeyvalue_heap(1, &heap_ctx);
 
-        add_record2_heap(&heap_ctx, "abc", "def");
-        add_record2_heap(&heap_ctx, "abc", "def");
-        add_record2_heap(&heap_ctx, "abcd", "ddef");
+        append_dictionary(&heap_ctx);
+
+        for(int i = 0; i < heap_ctx.heap_size - 1; i++) {
+                if (strcmp(heap_ctx.keyvalptr[i].key, strname) == 0 && found == 0) {
+                        found = 1;
+
+                        printf("%s\n\n", strname);
+
+                        printf("%s\n", heap_ctx.keyvalptr[i].value);
+                } 
+        }
+
+        if (found == 0) {
+                printf("%s\n", "function not found");
+        }
         
         clear_heap(&heap_ctx);
 }
