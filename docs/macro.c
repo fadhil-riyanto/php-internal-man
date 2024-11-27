757ae8a87ee98eea657fb3c21d67526425d97d7e
fadhil riyanto <me@fadev.org>
2024-11-28 06:39:41 +0700

    update docs

--- docs/macro.c
+++ docs/macro.c
@@ -54,11 +54,75 @@ struct value_data mc_ZVAL_STR_COPY = {
         .see_also = NULL
 };
 
+
+struct value_data mc_Z_TYPE = {
+        .synopsis = "Z_TYPE() macro for retrieving the type tag",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php5/zvals/basic_structure.html",
+        .examples = "zval zv;\n" \
+                        "\tzval *zv_ptr;\n" \
+                        "\tzval **zv_ptr_ptr;\n" \
+                        "\tzval ***zv_ptr_ptr_ptr;\n" \
+                        "\t\n" \
+                        "\tZ_TYPE(zv);                 // = zv.type\n" \
+                        "\tZ_TYPE_P(zv_ptr);           // = zv_ptr->type\n" \
+                        "\tZ_TYPE_PP(zv_ptr_ptr);      // = (*zv_ptr_ptr)->type\n" \
+                        "\tZ_TYPE_PP(*zv_ptr_ptr_ptr); // = (**zv_ptr_ptr_ptr)->type\n",
+        .see_also = NULL
+};
+
+
+struct value_data mc_Z_TYPE_P = {
+        .synopsis = "Z_TYPE_P() macro for retrieving the type tag of zval*",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php5/zvals/basic_structure.html",
+        .examples = "zval zv;\n" \
+                        "\tzval *zv_ptr;\n" \
+                        "\tzval **zv_ptr_ptr;\n" \
+                        "\tzval ***zv_ptr_ptr_ptr;\n" \
+                        "\t\n" \
+                        "\tZ_TYPE(zv);                 // = zv.type\n" \
+                        "\tZ_TYPE_P(zv_ptr);           // = zv_ptr->type\n" \
+                        "\tZ_TYPE_PP(zv_ptr_ptr);      // = (*zv_ptr_ptr)->type\n" \
+                        "\tZ_TYPE_PP(*zv_ptr_ptr_ptr); // = (**zv_ptr_ptr_ptr)->type\n",
+        .see_also = NULL
+};
+
+struct value_data mc_Z_TYPE_PP = {
+        .synopsis = "Z_TYPE_PP() macro for retrieving the type tag of zval**",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php5/zvals/basic_structure.html",
+        .examples = "zval zv;\n" \
+                        "\tzval *zv_ptr;\n" \
+                        "\tzval **zv_ptr_ptr;\n" \
+                        "\tzval ***zv_ptr_ptr_ptr;\n" \
+                        "\t\n" \
+                        "\tZ_TYPE(zv);                 // = zv.type\n" \
+                        "\tZ_TYPE_P(zv_ptr);           // = zv_ptr->type\n" \
+                        "\tZ_TYPE_PP(zv_ptr_ptr);      // = (*zv_ptr_ptr)->type\n" \
+                        "\tZ_TYPE_PP(*zv_ptr_ptr_ptr); // = (**zv_ptr_ptr_ptr)->type\n",
+        .see_also = NULL
+};
+
+
 inline void append_macro_section(struct heap_control *ptr)
 {
         add_record2_heap(ptr, "ZEND_PARSE_PARAMETERS_START", &mc_ZEND_PARSE_PARAMETERS_START);
         add_record2_heap(ptr, "ZEND_PARSE_PARAMETERS_END", &mc_ZEND_PARSE_PARAMETERS_END);
         add_record2_heap(ptr, "ZVAL_STR_COPY", &mc_ZVAL_STR_COPY);
+        
+        add_record2_heap(ptr, "Z_TYPE", &mc_Z_TYPE);
+        add_record2_heap(ptr, "Z_TYPE_P", &mc_Z_TYPE_P);
+        add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
         
 
 }
