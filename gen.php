2654de7a04e868ba3dfcd61b03839521817d99c0
fadhil riyanto <me@fadev.org>
2024-11-28 06:25:08 +0700

    update generator

--- gen.php
+++ gen.php
@@ -2,6 +2,15 @@
 
 $input_data = file_get_contents("notepad");
 $arr = explode("\n", $input_data);
+
+$first = 1;
 foreach($arr as $i) {
-        echo "\"" . $i . "\\n\" \\" . PHP_EOL;
+        if ($first == 1) {
+                echo "\"" . $i . "\\n\" \\" . PHP_EOL;
+        } else {
+                echo "                        \"\\t" . $i . "\\n\" \\" . PHP_EOL;
+        }
+        
+
+        $first = 0;
 }
