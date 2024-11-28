ecbfadbbb86c1037b0d10ac26bdd4ac362ed63b0
fadhil riyanto <me@fadev.org>
2024-11-28 07:22:59 +0700

    update data

--- docs/macro.c
+++ docs/macro.c
@@ -113,6 +113,63 @@ struct value_data mc_Z_TYPE_PP = {
         .see_also = NULL
 };
 
+struct value_data mc_Z_LVAL_P = {
+        .synopsis = "Z_LVAL_P() to get the long (integer) value",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php5/zvals/basic_structure.html",
+        .examples = "zval *zv_ptr = /* ... */;\n" \
+                        "\t\n" \
+                        "\tif (Z_TYPE_P(zv_ptr) == IS_LONG) {\n" \
+                        "\t    php_printf(\"Zval is a long with value %ld\\n\", Z_LVAL_P(zv_ptr));\n" \
+                        "\t} else /* ... */\n" ,
+        .see_also = NULL
+};
+
+
+struct value_data mc_ZSTR_VAL = {
+        .synopsis = "ZSTR_VAL() get string from zend_string *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/internal_types/strings/zend_strings.html",
+        .examples = "size_t len = 40;\n" \
+                        "\tzend_string *str = zend_string_alloc(len, /* persistent */ 0);\n" \
+                        "\tfor (size_t i = 0; i < len; i++) {\n" \
+                        "\t    ZSTR_VAL(str)[i] = 'a';\n" \
+                        "\t}\n" \
+                        "\t// Don't forget to null-terminate!\n" \
+                        "\tZSTR_VAL(str)[len] = '\0';\n" \
+                        "\t\n",
+        .see_also = NULL
+};
+
+
+struct value_data mc_ZSTR_LEN = {
+        .synopsis = "ZSTR_LEN() get string length from zend_string *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/internal_types/strings/zend_strings.html",
+        .examples = "zval val;\n" \
+                        "\tZVAL_STRING(&val, \"foo\");\n" \
+                        "\t\n" \
+                        "\t// Z_STRLEN, Z_STRVAL work on zval.\n" \
+                        "\tphp_printf(\"string(%zd) \\\"%s\\\"\\n\", Z_STRLEN(val), Z_STRVAL(val));\n" \
+                        "\t\n" \
+                        "\t// ZSTR_LEN, ZSTR_VAL work on zend_string.\n" \
+                        "\tzend_string *str = Z_STR(val);\n" \
+                        "\tphp_printf(\"string(%zd) \\\"%s\\\"\\n\", ZSTR_LEN(str), ZSTR_VAL(str));\n" \
+                        "\t\n" \
+                        "\tzval_ptr_dtor(&val);\n" \
+                        "\t\n" ,
+        .see_also = NULL
+};
+
 
 inline void append_macro_section(struct heap_control *ptr)
 {
@@ -123,6 +180,15 @@ inline void append_macro_section(struct heap_control *ptr)
         add_record2_heap(ptr, "Z_TYPE", &mc_Z_TYPE);
         add_record2_heap(ptr, "Z_TYPE_P", &mc_Z_TYPE_P);
         add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
+        
+        add_record2_heap(ptr, "Z_LVAL_P", &mc_Z_LVAL_P);
+        add_record2_heap(ptr, "ZSTR_VAL", &mc_ZSTR_VAL);
+        add_record2_heap(ptr, "ZSTR_LEN", &mc_ZSTR_LEN);
+        // add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
+        // add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
+        // add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
+        // add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
+        // add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
         
 
 }
