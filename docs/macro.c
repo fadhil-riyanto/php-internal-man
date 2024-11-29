706a5407bbaaebf68c05a86ccb50b3c25eba0e6c
fadhil riyanto <me@fadev.org>
2024-11-29 23:56:41 +0700

    update docs

--- docs/macro.c
+++ docs/macro.c
@@ -171,6 +171,224 @@ struct value_data mc_ZSTR_LEN = {
 };
 
 
+
+struct value_data mc_Z_PARAM_ARRAY = {
+        .synopsis = "Z_PARAM_ARRAY() get array from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_ARRAY_OR_OBJECT = {
+        .synopsis = "Z_PARAM_ARRAY_OR_OBJECT() get array or obj from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_BOOL = {
+        .synopsis = "Z_PARAM_BOOL() get boolean from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+
+struct value_data mc_Z_PARAM_CLASS = {
+        .synopsis = "Z_PARAM_CLASS() get class obj callee from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+
+struct value_data mc_Z_PARAM_DOUBLE = {
+        .synopsis = "Z_PARAM_DOUBLE() get double from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+
+struct value_data mc_Z_PARAM_FUNC = {
+        .synopsis = "Z_PARAM_FUNC() get function callee from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_ARRAY_HT = {
+        .synopsis = "Z_PARAM_ARRAY_HT() get array hashtable from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_ARRAY_OR_OBJECT_HT = {
+        .synopsis = "Z_PARAM_ARRAY_OR_OBJECT_HT() get array or object hashtable from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_LONG = {
+        .synopsis = "Z_PARAM_LONG() get int from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_STRICT_LONG = {
+        .synopsis = "Z_PARAM_STRICT_LONG() get string long from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+
+struct value_data mc_Z_PARAM_OBJECT = {
+        .synopsis = "Z_PARAM_OBJECT() get object from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_OBJECT_OF_CLASS = {
+        .synopsis = "Z_PARAM_OBJECT_OF_CLASS() get object or class from function parameters *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+
+struct value_data mc_Z_PARAM_PATH = {
+        .synopsis = "Z_PARAM_PATH() (pending docs) *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_PATH_STR = {
+        .synopsis = "Z_PARAM_PATH_STR() (pending docs) *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_RESOURCE = {
+        .synopsis = "Z_PARAM_RESOURCE() (pending docs) *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+
+struct value_data mc_Z_PARAM_STRING = {
+        .synopsis = "Z_PARAM_STRING() get string from php function *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_STR = {
+        .synopsis = "Z_PARAM_STR() (pending docs) *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+struct value_data mc_Z_PARAM_ZVAL = {
+        .synopsis = "Z_PARAM_ZVAL() (pending docs) *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+
+struct value_data mc_Z_PARAM_VARIADIC = {
+        .synopsis = "Z_PARAM_VARIADIC() get multiple param at once *",
+        .param = NULL,
+        .param_desc = NULL,
+        .desc = NULL,
+        .return_value = "void",
+        .reference = "https://www.phpinternalsbook.com/php7/extensions_design/php_functions.html",
+        .examples = NULL,
+        .see_also = NULL
+};
+
+
 inline void append_macro_section(struct heap_control *ptr)
 {
         add_record2_heap(ptr, "ZEND_PARSE_PARAMETERS_START", &mc_ZEND_PARSE_PARAMETERS_START);
@@ -184,6 +402,26 @@ inline void append_macro_section(struct heap_control *ptr)
         add_record2_heap(ptr, "Z_LVAL_P", &mc_Z_LVAL_P);
         add_record2_heap(ptr, "ZSTR_VAL", &mc_ZSTR_VAL);
         add_record2_heap(ptr, "ZSTR_LEN", &mc_ZSTR_LEN);
+
+        add_record2_heap(ptr, "Z_PARAM_ARRAY", &mc_Z_PARAM_ARRAY);
+        add_record2_heap(ptr, "Z_PARAM_ARRAY_OR_OBJECT", &mc_Z_PARAM_ARRAY_OR_OBJECT);
+        add_record2_heap(ptr, "Z_PARAM_BOOL", &mc_Z_PARAM_BOOL);
+        add_record2_heap(ptr, "Z_PARAM_CLASS", &mc_Z_PARAM_CLASS);
+        add_record2_heap(ptr, "Z_PARAM_DOUBLE", &mc_Z_PARAM_DOUBLE);
+        add_record2_heap(ptr, "Z_PARAM_FUNC", &mc_Z_PARAM_FUNC);
+        add_record2_heap(ptr, "Z_PARAM_ARRAY_HT", &mc_Z_PARAM_ARRAY_HT);
+        add_record2_heap(ptr, "Z_PARAM_ARRAY_OR_OBJECT_HT", &mc_Z_PARAM_ARRAY_OR_OBJECT_HT);
+        add_record2_heap(ptr, "Z_PARAM_LONG", &mc_Z_PARAM_LONG);
+        add_record2_heap(ptr, "Z_PARAM_STRICT_LONG", &mc_Z_PARAM_STRICT_LONG);
+        add_record2_heap(ptr, "Z_PARAM_OBJECT", &mc_Z_PARAM_OBJECT);
+        add_record2_heap(ptr, "Z_PARAM_OBJECT_OF_CLASS", &mc_Z_PARAM_OBJECT_OF_CLASS);
+        add_record2_heap(ptr, "Z_PARAM_PATH", &mc_Z_PARAM_PATH);
+        add_record2_heap(ptr, "Z_PARAM_PATH_STR", &mc_Z_PARAM_PATH_STR);
+        add_record2_heap(ptr, "Z_PARAM_RESOURCE", &mc_Z_PARAM_RESOURCE);
+        add_record2_heap(ptr, "Z_PARAM_STRING", &mc_Z_PARAM_STRING);
+        add_record2_heap(ptr, "Z_PARAM_STR", &mc_Z_PARAM_STR);
+        add_record2_heap(ptr, "Z_PARAM_ZVAL", &mc_Z_PARAM_ZVAL);
+        add_record2_heap(ptr, "Z_PARAM_VARIADIC" , &mc_Z_PARAM_VARIADIC);
         // add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
         // add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
         // add_record2_heap(ptr, "Z_TYPE_PP", &mc_Z_TYPE_PP);
